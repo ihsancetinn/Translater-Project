c74a06d69565b30d1a62447e1d9bd9d0eb48b569
ihsancetinn <ihsancetin92@gmail.com>
2022-08-23 14:41:11 +0300

    translate çalışması

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-const api_key = "964496f98ebc8f9f93f6d140";
-const url = " https://v6.exchangerate-api.com/v6/" + api_key;
+const fromLang = document.querySelector("#from-lang");
+const toLang = document.querySelector("#to-lang");
+const btnTranslate = document.querySelector("#btnTranslate");
+const fromText = document.querySelector("#from-text");
+const toText = document.querySelector("#to-text");
+
+
+for (let lang in languages) {
+    // console.log(lang, languages[lang]);
+
+    let option = `
+    <option value="${lang}">${languages[lang]}</option>`;
+    fromLang.insertAdjacentHTML("beforeend", option);
+    toLang.insertAdjacentHTML("beforeend", option);
+
+
+    fromLang.value = "tr-TR";
+    toLang.value = "en-GB";
+}
+
+btnTranslate.addEventListener("click", () => {
+    let text = fromText.value;
+    let from = fromLang.value;
+    let to = toLang.value;
+    const url = `https://api.mymemory.translated.net/get?q=${text}&langpair=${from}|${to}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            toText.value = data.responseData.translatedText
+        });
+        
+})
