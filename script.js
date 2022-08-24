974cd4e3a7ef3d62ba6d9518383d79ea5d9d86c3
ihsancetinn <ihsancetin92@gmail.com>
2022-08-24 16:45:19 +0300

    copy ve languages atribute

--- script.js
+++ script.js
@@ -35,18 +35,36 @@ btnTranslate.addEventListener("click", () => {
 exchange.addEventListener('click', () => {
 
     let text = fromText.value;
-    fromText.value= toText.value;
+    fromText.value = toText.value;
     toText.value = text;
 
     let lang = fromLang.value;
 
-    fromLang.value= toLang.value;
-    toLang.value= lang;
+    fromLang.value = toLang.value;
+    toLang.value = lang;
 
 });
 
-for(let icon of icons){
-    icon.addEventListener('click', (element)=>{
+for (let icon of icons) {
+    icon.addEventListener('click', (element) => {
+        if (element.target.classList.contains("fa-copy")) {
+            if (element.target.id === 'from') {
+                navigator.clipboard.writeText(fromText.value);
+            } else {
+                navigator.clipboard.writeText(toText.value);
+            }
+        } else {
+            let utterance;
+
+            if (element.target.id === 'from') {
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang =fromLang.value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang =toLang.value;
+            }
+            speechSynthesis.speak(utterance);
+        }
 
     })
 }
